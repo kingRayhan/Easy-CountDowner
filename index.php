1efab42906dcf2f502fbf678647df7c3fed9e265
kingrayhan <rayhan095@gmail.com>
2016-01-30 18:12:04 +0600

    Easy countdowner file upload

---
+++ index.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * Plugin Name: Easy CountDowner
+ * Description: An awesome countdown shortcode plugin. Just click on the countDowner button at tinyMce editor , provide settings information and click ok.
+ * Plugin URI: plugin.rayhan.info
+ * Author: Rayhan
+ * Author URI: https://www.facebook.com/rayhan095
+ * Version: 1.0.5
+ * License: GPL2
+ *
+ */
+
+/**
+ * Copyright (c) 2015 | rayhan095@gmail.com | All rights reserved.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * **********************************************************************
+ */
+function easy_countDowner_file(){
+	
+	 
+	//----------------------------------------------
+	//	Include king Countdowner css file
+	//----------------------------------------------
+	wp_register_style( 'king-countdowner', plugin_dir_url(__FILE__).'assets/TimeCircles.css', '', '1.0', 'all' );
+	wp_enqueue_style('king-countdowner');
+	
+	
+	//----------------------------------------------
+	//	Include king Countdowner javascript file
+	//----------------------------------------------
+	wp_enqueue_script('jquery');
+	wp_enqueue_script( 'king-countdowner-js',  plugin_dir_url(__FILE__).'assets/TimeCircles.js', array('jquery'), '1.0', false );
+	
+}
+add_action('wp_enqueue_scripts','easy_countDowner_file');
+
+
+function easy_countDowner_shortcode($atts,$content){
+	ob_start();
+	
+		$easy_countDowner =shortcode_atts(array(
+			'name' => '',
+			'animation' => 'smooth',
+			'end_date' => '',
+			'end_time' => '00:00:00',
+			'circle_bg_color' => '#E2E2E2',
+			
+			//themes
+			'theme' => 'default',
+			
+			
+			'day_label' => 'Days',
+			'day_color' => '#60686F',
+			'show_day' => true,
+			
+			'hour_label' => 'Hours',
+			'hour_color' => '#60686F',
+			'show_hour' => true,
+			
+			'minute_label' => 'Minutes',
+			'minute_color' => '#60686F',
+			'show_minute' => true,
+			
+			'second_label' => 'Seconds',
+			'second_color' => '#60686F',
+			'show_second' => true
+		),$atts);
+		
+	?>
+	
+	<div id="<?php echo $easy_countDowner['name']; ?>" style="width: 100%;" data-date="<?php echo $easy_countDowner['end_date'] ?> <?php echo $easy_countDowner['end_time'] ?>"></div>
+	
+	<script type="text/javascript">
+		jQuery("#<?php echo $easy_countDowner['name']; ?>").TimeCircles({
+			"animation": "<?php echo $easy_countDowner['animation'] ?>",
+			<?php if( $easy_countDowner['theme'] == 'default' ){ ?>
+				"bg_width": 0.2,
+				"fg_width": 0.043333333333333335,
+			<?php }elseif( $easy_countDowner['theme'] == 'fat' ){ ?>
+				    "bg_width": 1.7,
+					"fg_width": 0.09333333333333334,
+			<?php }elseif( $easy_countDowner['theme'] == 'thick' ){ ?>
+					"bg_width": 1.5,
+					"fg_width": 0.04666666666666667,
+			<?php } ?>
+			"circle_bg_color": "<?php echo $easy_countDowner['circle_bg_color'] ?>",
+			"time": {
+				"Days": {
+					"text": "<?php echo $easy_countDowner['day_label'] ?>",
+					"color": "<?php echo $easy_countDowner['day_color'] ?>",
+					"show": <?php echo $easy_countDowner['show_day'] ?>
+				},
+				"Hours": {
+					"text": "<?php echo $easy_countDowner['hour_label'] ?>",
+					"color": "<?php echo $easy_countDowner['hour_color'] ?>",
+					"show": <?php echo $easy_countDowner['show_hour'] ?>
+				},
+				"Minutes": {
+					"text": "<?php echo $easy_countDowner['minute_label'] ?>",
+					"color": "<?php echo $easy_countDowner['minute_color'] ?>",
+					"show": <?php echo $easy_countDowner['show_minute'] ?>
+				},
+				"Seconds": {
+					"text": "<?php echo $easy_countDowner['second_label'] ?>",
+					"color": "<?php echo $easy_countDowner['second_color'] ?>",
+					"show": <?php echo $easy_countDowner['show_second'] ?>
+				}
+			}
+		});
+		<?php $countdowner_id++; ?>
+	</script>
+	
+<?php
+	$var_ = ob_get_clean();
+	return $var_;
+}
+add_shortcode('easy_countdowner','easy_countDowner_shortcode');
+// Support shortcode in Text Widget
+add_filter('widget_text', 'do_shortcode');
+
+
+
+// Hooks your functions into the correct filters
+function my_add_mce_button() {
+	// check user permissions
+	if ( !current_user_can( 'edit_posts' ) && !current_user_can( 'edit_pages' ) ) {
+		return;
+	}
+	// check if WYSIWYG is enabled
+	if ( 'true' == get_user_option( 'rich_editing' ) ) {
+		add_filter( 'mce_external_plugins', 'my_add_tinymce_plugin' );
+		add_filter( 'mce_buttons', 'my_register_mce_button' );
+	}
+}
+add_action('admin_head', 'my_add_mce_button');
+
+// Declare script for new button
+function my_add_tinymce_plugin( $plugin_array ) {
+	$plugin_array['my_mce_button'] = plugin_dir_url(__FILE__).'assets/shortcode_generator.js';
+	return $plugin_array;
+}
+
+// Register new button in the editor
+function my_register_mce_button( $buttons ) {
+	array_push( $buttons, 'my_mce_button' );
+	return $buttons;
+}
+
+function easy_countDowner_shortcodes_mce_css() {
+	wp_enqueue_style('symple_shortcodes-tc', plugins_url('/assets/shortcode-generate_icon.css', __FILE__) );
+}
+add_action( 'admin_enqueue_scripts', 'easy_countDowner_shortcodes_mce_css' );
---
+++ readme.txt
@@ -0,0 +1,90 @@
+=== Easy CountDowner ===
+Contributors: kingrayhan
+Tags: CountDown,count Down,Countdowner,countdowner,count,easy-countdowner,easy-count-downer,EasyCountDowner
+Requires at least: 4.0
+Tested up to: 4.4.0
+Stable tag: 1.0.5
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A countDown plugin for wordpress which is works through shortcode!
+
+== Description ==
+Easy Countdowner allows you to display a countdown on your post or page. The easiest way to place your full customizable HTML5 Countdown Timer. Just click on the countDowner button at tinyMce editor , provide settings information and click ok.
+
+= List of features: =
+* No flash, only javascript!
+* Customize of colors for text and background
+* Set the texts for date, time
+* Simple using shortcode
+* Add countdown timers from Widgets area using shortcode
+* Modern Style full customizable
+* Unlimited countdown timers on the same page/post
+* Add multiple countdown timers from Widgets area
+* Use On Unlimited Sites
+
+
+
+= Tested on =
+* Mac Firefox 	:)
+* Mac Safari 	:)
+* Mac Chrome	:)
+* PC Safari 	:)
+* PC Chrome		:)
+* PC Firefox	:)
+* iPhone Safari :)
+* iPad Safari 	:)
+* PC ie7		:S
+
+> [Demos and Documentation ](http://plugin.rayhan.info/easy-countdowner/)
+
+== Installation ==
+
+= For automatic installation: =
+* The simplest way to install is to click on 'Plugins' then 'Add New' and type 'Easy CountDowner' in the search field.
+* Click 'Install Now' and have Fun!
+
+= For manual installation: =
+* Login to your website and go to the Plugins section of your admin panel.
+* Click the Add New button.
+* Under Install Plugins, click the Upload link.
+* Select the plugin zip file (easy-CountDowner.zip) from your computer then click the Install Now button.
+* You should see a message stating that the plugin was installed successfully.
+* Click the Activate Plugin link.
+
+
+= Installation from server: =
+* You should have access to the server where WordPress is installed. If you don't, see your system administrator.
+* Copy the plugin zip file (easy-CountDowner.zip) up to your server and unzip it somewhere on the file system.
+* Copy the "easy-CountDowner" folder into the /wp-content/plugins directory of your WordPress installation.
+* Login to your website and go to the Plugins section of your admin panel.
+* Look for "Easy CountDowner" and click Activate.
+== Screenshots ==
+
+1. Easy CountDowner Shortcode generator button
+
+2. After click countDowner Shortcode generator button this popup will be appear. Put countdowner settings here and click ok.
+
+3. CountDowner Demo 1
+
+4. CountDowner Demo 2
+
+5. CountDowner Demo 3
+
+6. CountDowner Demo 4
+
+7. CountDowner Demo 5
+
+8. CountDowner Demo 6
+
+9. CountDowner Demo 7
+
+10. CountDowner Demo 8
+
+11. CountDowner Demo 9
+
+12. CountDowner Demo 10
+
+13. CountDowner Demo 11
+
+14. CountDowner Demo 13
